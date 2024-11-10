8e8d103ecb6c1e0d350c6bd6787478de37e16b63
Johannes Eimer <johannes.eimer@jep-dev.com>
2024-11-10 17:40:44 +0100

    fix: add missing parameter

--- core/tabs/system-setup/arch/server-setup.sh
+++ core/tabs/system-setup/arch/server-setup.sh
@@ -404,7 +404,7 @@ elif [[ "${FS}" == "ext4" ]]; then
 elif [[ "${FS}" == "luks" ]]; then
     mkfs.vfat -F32 -n "EFIBOOT" "${partition2}"
 # enter luks password to cryptsetup and format root partition
-    echo -n "${LUKS_PASSWORD}" | cryptsetup -v luksFormat "${partition3}" -
+    echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat "${partition3}" -
 # open luks container and ROOT will be place holder
     echo -n "${LUKS_PASSWORD}" | cryptsetup open "${partition3}" ROOT -
 # now format that container
